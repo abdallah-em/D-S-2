113e27aab3253ea49952c00b4fd5da4daa6df7fc
Abdallah-em <omdaabood@gmail.com>
2020-07-05 21:59:22 +0200

    tested Stack

--- DS--2/DS--2/DS--2.cpp
+++ DS--2/DS--2/DS--2.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 int main()
 {
-	
+	Stack st;
+	st.push(10);
+	st.push(15);
+	st.push(20);
+	st.push(25);
+	cout << st.top() << endl;
+	st.pop();
+	cout << st.top() << endl;
+	st.pop();
+	cout << st.top() << endl;
+	st.pop();
+	cout << st.top() << endl;
+	st.pop();
+
+	if (st.empty() == true)
+	{
+		cout << "5lawees" << endl;
+	}
+	else
+	{
+		st.pop();
+		cout << st.top();
+	}
 	
 	return 0;
 }
